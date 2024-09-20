0bf8131131d9544990adcf12e7902ca7c227ec8d
abdielbytes <wilsonabdiel86@gmail.com>
2024-09-20 15:22:03 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Note\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +26,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('notes', NoteController::class);
