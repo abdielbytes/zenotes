ec6122f767fb99b2b8034f289801f3b5e100db44
abdielbytes <wilsonabdiel86@gmail.com>
2024-09-21 23:50:51 +0100

    wip

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -6,6 +6,11 @@ import { PageProps as AppPageProps } from './';
 declare global {
     interface Window {
         axios: AxiosInstance;
+        SpeechRecognition: typeof SpeechRecognition;
+        webkitSpeechRecognition: typeof SpeechRecognition;
+        text: typeof text;
+        char: typeof char;
+        result: typeof any;
     }
 
     var route: typeof ziggyRoute;
